2aee106213b67a1dd4028ba0b2aa878d4d89dbb6
3rekse <bonfiglio.fabrizio@itiomar.net>
2024-12-09 09:34:36 +0100

    canvas in globals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HexagonCanvas from './components/HexagonCanvas';
 const Home: React.FC = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center' }}>
-      <div>
+      <div >
       <h1 style={{ fontFamily: 'Mountains of Christmas, cursive' }}>GAME OMAR Buone Vacanze 2024 dall' I.T.I "Giuseppe Omar" Novara</h1>
       <HexagonCanvas />
       </div>
